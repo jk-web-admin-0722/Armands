210d0e6d313d0711f2378dd1eefc678a599bcec2
jkarmands <75387346+jkarmands@users.noreply.github.com>
2022-07-02 10:48:20 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web_Lapa</title>
+</head>
+<body>
+    <h1>Virsraksts1</h1>
+    <h2>Virsraksts2</h2>
+    <h3>Virsraksts3</h3>
+    <h4>Virsraksts4</h4>
+    <h5>Virsraksts5</h5>
+    <h6>Virsraksts6</h6>
+    
+
+    <p>Sveiks manā lapā
+        <p>Lorem ipsum dolor sit amet, <b>consectetur adipisicing elit. Quos eaque sit, voluptate</b> nobis labore ea rerum iste porro enim suscipit recusandae. Aliquam repellendus vel doloremque temporibus voluptatum quaerat, in consequatur!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, eum enim accusamus maiores in molestias nisi quasi, ratione vitae dolorem facere incidunt cum nulla, perferendis maxime sint numquam rem?
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. <i><b>Vel magni tempora quaerat velit eligendi. Voluptas</i></b> numquam magnam deserunt rem, voluptates adipisci sit qui provident sed vitae beatae, repudiandae animi praesentium?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit id, <strong>cupiditate accusamus</strong> rem perspiciatis commodi, <em>vero eaque est aut fuga adipisci veritatis</em> voluptatibus eum quis sit quaerat nam repellendus!</p>
+            
+        <!-- Teksts, Kurš neparādīsies-->>
+
+        <p></p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam vitae tenetur, suscipit eligendi laborum iure quae ducimus explicabo fugit. Ratione fugit provident officia, quibusdam praesentium ducimus voluptatem dolorem architecto adipisci?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit adipisci aut eveniet exercitationem libero quisquam nesciunt, labore minus dolor recusandae et vel fuga accusamus esse mollitia laborum expedita excepturi. Asperiores!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A cupiditate consequatur ipsa possimus nesciunt accusamus, temporibus fugiat tenetur voluptatum illum voluptatibus obcaecati nisi expedita totam beatae sint inventore. Soluta, quae!</p>
+    
+        <!-- Unordered list, aizzīmēts saraksts-->
+    <ul>
+        <li>Ozols</li>
+        <li>Osis</li>
+        <li>Apse</li>
+    </ul>
+        <!-- Ordered list, numurēts saraksts-->
+    <ol>
+        <li>Ozols</li>
+        <li>Osis</li>
+        <li>Apse</li>
+    </ol>
+
+    <a href="#">Nospied mani!</a>
+    <a href="www.google.lv">Meklē šeit!</a>
+    </body>
+</body>
+</html>
